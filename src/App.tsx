057070bb75163c58c41d227a6d0f994c0f7751bf
gracelai1290 <gracelai1290@gmail.com>
2025-09-01 16:10:02 -0700

    Fix React Router basename to work correctly with GitHub Pages base path

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ function AppContent() {
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <SearchProvider>
         <CategoryFilterProvider>
           <AppContent />
