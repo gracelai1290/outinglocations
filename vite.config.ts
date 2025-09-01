77783b7eca705d8b94e17563c36b4bde057ac72d
gracelai1290 <gracelai1290@gmail.com>
2025-09-01 15:46:03 -0700

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig(({ command, mode }) => {
   return {
     plugins: [react()],
     // base: isProduction ? '/outinglocations/' : '/'
-    base: '/outinglocations/'
+    base: '/outinglocations/',
   }
 })
