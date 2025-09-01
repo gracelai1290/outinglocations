95100d1ecee75c6e5f9bcd9c72643b29f9af8ef7
gracelai1290 <gracelai1290@gmail.com>
2025-09-01 15:32:06 -0700

    update web file

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'build' ? '/outinglocations/' : '/'
-}))
+  // Temporarily remove base path for development
+  // base: '/outinglocations/'
+})
